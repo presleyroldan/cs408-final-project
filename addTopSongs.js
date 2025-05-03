67b39f222a4b07ff36f44cd01fdb4c6599e0bbfd
presleyroldan <108436829+presleyroldan@users.noreply.github.com>
2025-05-03 15:12:15 -0600

    another script fix

--- addTopSongs.js
+++ addTopSongs.js
@@ -1,22 +1,22 @@
 const { sequelize, Song } = require('./backend/models'); 
 
-const topSongs = [
-  { title: 'Luther', artist: 'Jean Dawson', album: 'CHAOS NOW', genre: 'Alternative', length: '2:57' },
-  { title: 'Die With A Smile', artist: 'HUNNY', album: 'Homesick', genre: 'Indie Rock', length: '3:02' },
-  { title: 'NOKIA', artist: 'Jean Dawson', album: 'CHAOS NOW*', genre: 'Alternative', length: '2:45' },
-  { title: 'Pink Pony Club', artist: 'Chappell Roan', album: 'The Rise and Fall of a Midwest Princess', genre: 'Pop', length: '4:01' },
-  { title: 'Just In Case', artist: 'Duckwrth', album: 'Chrome Bull', genre: 'Hip-Hop', length: '3:28' },
-  { title: '30 For 30', artist: 'Drake', album: 'What A Time To Be Alive', genre: 'Rap', length: '4:13' },
-  { title: 'All The Way', artist: 'Timeflies', album: 'Just for Fun', genre: 'Pop', length: '3:28' },
-  { title: 'Ordinary', artist: 'Umi', album: 'Forest in the City', genre: 'R&B/Soul', length: '3:18' },
-  { title: 'Gimme A Hug', artist: 'Coach Party', album: 'Killjoy', genre: 'Alt Rock', length: '2:50' },
-  { title: 'Smile', artist: 'Wolf Alice', album: 'Blue Weekend', genre: 'Rock', length: '3:15' }
+const songs = [
+  { title: 'Luther', artist: 'Kendrick Lamar & SZA', length: '3:45', genre: 'Hip-Hop', album: 'CTRL+Section' },
+  { title: 'Die With A Smile', artist: 'Lady Gaga & Bruno Mars', length: '4:05', genre: 'Pop', album: 'Neon Lovers' },
+  { title: 'NOKIA', artist: 'Drake', length: '3:30', genre: 'Rap', album: 'Silicon Dreams', length: '2:45' },
+  { title: 'Pink Pony Club', artist: 'Chappell Roan', length: '3:50', genre: 'Alt-Pop', album: 'Midwest Princess' },
+  { title: 'Just In Case', artist: 'Morgan Wallen', length: '3:40', genre: 'Country', album: 'Whiskey Hours' },
+  { title: '30 For 30', artist: 'SZA with Kendrick Lamar', length: '4:00', genre: 'R&B', album: 'Duality' },
+  { title: 'All The Way', artist: 'BigXthaPlug featuring Bailey Zimmerman', length: '3:55', genre: 'Trap/Country', album: 'Southbound Heat' },
+  { title: 'Ordinary', artist: 'Alex Warren', length: '3:35', genre: 'Pop Rock', album: 'Bittersweet' },
+  { title: 'Gimme A Hug', artist: 'Drake', length: '3:25', genre: 'Hip-Hop', album: 'Certified Loner Boy' },
+  { title: 'Smile', artist: 'Morgan Wallen', length: '3:45', genre: 'Country', album: 'Rust Belt Heart' }
 ];
 
 async function addSongs() {
   try {
-    await sequelize.sync(); // just in case
-    for (const song of topSongs) {
+    await sequelize.sync(); 
+    for (const song of songs) {
       await Song.create(song);
       console.log(`✅ Added: ${song.title}`);
     }
